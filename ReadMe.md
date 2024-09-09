4ffac021c7515ae4a43f597069644b43ec02f878
MalBourbie <MalBourbie@gmail.com>
2024-09-09 17:28:45 +0200

    function changeSlide, handleSlideChange

--- ReadMe.md
+++ ReadMe.md
@@ -6,6 +6,12 @@ dont .arrow, .arrow_left, .arrow_right.
 -ajout des classes aux flèches ajouter dans le html plus tôt.
 
 -récupération des deux flèches par un querySelectorAll.
--création fonction handleClick qui console.log l'élément passé en paramètre.
+-création fonction handleSlideChange qui console.log l'élément passé en paramètre.
 -ajout d'une boucle forEach pour parcourir les 2 flèches selectionnées au click,
-qui appelle la fonction handleClick.
+qui appelle la fonction handleSlideChange.
+
+-création d'une div avec la class "dot" pour le nombre de slides dans cette même const.
+-variable currentSlideNumber set à 0 pour savoir sur quelle slide on est.
+-handleSlideChange qui vérifie sur quelle flèche l'utilisateur à cliquer.
+Si c'est la flèche de droite on rajoute 1 à "currentSlideNumber", ou enlève 1 si ce n'est pas le cas. Et appelle la fonction changeSlide
+-La fonction changeSlide change le src de la balise img enlève la précédente classe "dot_selected" puis ajoute cette classe au "dot" numéro "currentSlideNumber".
--- assets/script.js
+++ assets/script.js
@@ -17,13 +17,40 @@ const slides = [
 	}
 ]
 
+// arrow
 const arrows = document.querySelectorAll("#banner .arrow") 
 arrows.forEach(arrow => {
 	arrow.addEventListener("click", () => {
-		handleClick(arrow)
+		handleSlideChange(arrow)
 	})
 })
 
-function handleClick(element) {
-	console.log(element)
+// dots
+const dots = document.querySelector(".dots")
+slides.forEach(() => {
+	dots.innerHTML += "<div class='dot'></div>"
+})
+
+// change slide
+let currentSlide = document.querySelector(".banner-img")
+const dot = document.querySelectorAll(".dot")
+let currentSlideNumber = 0
+dot[currentSlideNumber].classList.add("dot_selected") // set the first selected dot
+
+
+function handleSlideChange(element) {
+	if (element.classList.contains("arrow_right")) {
+		currentSlideNumber++
+		changeSlide()
+	} else {
+		currentSlideNumber--
+		changeSlide()
+	}
+}
+
+function changeSlide() {
+	currentSlide.src = "./assets/images/slideshow/" + slides[currentSlideNumber]["image"]
+	let previousDot = document.querySelector(".dot_selected")
+	previousDot.classList.remove("dot_selected")
+	dot[currentSlideNumber].classList.add("dot_selected")
 }
